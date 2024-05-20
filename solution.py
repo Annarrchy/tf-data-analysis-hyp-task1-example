466c67ad4e04b8050f404d32e9015d77c8ead62d
Annarrchy <91114932+Annarrchy@users.noreply.github.com>
2024-05-20 16:28:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1063441199 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    import scipy.stats as sps
+    alpha = 0.09
+
+    s_1 = np.concatenate((np.ones(x_success), np.zeros(x_cnt - x_success)))
+    s_2 = np.concatenate((np.ones(y_success), np.zeros(y_cnt - y_success)))
+    return sps.ttest_ind(s_1, s_2)[1] <= alpha
